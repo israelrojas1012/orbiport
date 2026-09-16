3543449b1f9448cfa6973de7a1709219aa28d8d7
israelrojas1012 <izraelrojas10@gmail.com>
2026-09-16 12:30:44 -0500

    Mejora: confirmar cierre de sesion en toda la app

--- mobile/src/pages/Home.jsx
+++ mobile/src/pages/Home.jsx
@@ -129,9 +129,11 @@ export default function Home() {
               strokeWidth="2"
               strokeLinecap="round"
               strokeLinejoin="round"
+              aria-hidden="true"
             >
-              <path d="M12 3v9" />
-              <path d="M18.36 6.64a9 9 0 1 1-12.73 0" />
+              <path d="M9 21H5a2 2 0 0 1-2-2V5a2 2 0 0 1 2-2h4" />
+              <path d="M16 17l5-5-5-5" />
+              <path d="M21 12H9" />
             </svg>
           </button>
         </div>
